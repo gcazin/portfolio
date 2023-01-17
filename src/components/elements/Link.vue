ee389fd6e72fdd12476dc560b9845e0db68d1aec
gcazin <czn.guillaume@gmail.com>
2023-01-17 22:06:50 +0100

    feat(views): CV pageÃ

---
+++ src/components/elements/Link.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="{ name: to }">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "Link",
+
+  props: {
+    to: {
+      type: String,
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,12 @@ export default createRouter({
     history: createWebHistory(),
     routes: [
         {
+            name: 'home',
             path: '/',
             component: Home,
         },
         {
+            name: 'cv',
             path: '/cv',
             component: CV,
         }
