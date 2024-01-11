dc7acb41833f3cbdda865f9e17e844c7a68f57e8
gcazin <czn.guillaume@gmail.com>
2024-01-11 14:07:27 +0100

    fix: background position

--- src/components/layout/Section.vue
+++ src/components/layout/Section.vue
@@ -72,6 +72,6 @@ export default {
 
 <style scoped>
 .background {
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23f1f5f9' fill-opacity='0.03' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23f1f5f9' fill-opacity='0.07' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
 }
 </style>
