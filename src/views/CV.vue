602be0f5701db0e530e03c6c6582c3ad89f709d2
gcazin <czn.guillaume@gmail.com>
2024-01-12 17:24:52 +0100

    fix: whoops wrong cv

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -10,63 +10,98 @@ import { onBeforeMount, onMounted, ref } from 'vue'
 
 const skills = [
     {
-        title: 'Relations clientèle',
+        title: 'HTML',
         rating: 'Maîtrise',
     },
     {
-        title: 'Aptitude aux chiffres',
+        title: 'CSS',
         rating: 'Maîtrise',
     },
     {
-        title: 'Sens de l\'organisation',
+        title: 'Javascript',
         rating: 'Maîtrise',
     },
     {
-        title: 'Maîtrise de l\'informatique',
+        title: 'VueJS 3',
         rating: 'Maîtrise',
     },
     {
-        title: 'Travail en équipe',
+        title: 'jQuery',
         rating: 'Maîtrise',
+    },
+    {
+        title: 'PHP',
+        rating: 'Maîtrise',
+    },
+    {
+        title: 'Laravel 9',
+        rating: 'Maîtrise',
+    },
+    {
+        title: 'SCSS',
+        rating: 'Connaissance',
+    },
+    {
+        title: 'Wordpress',
+        rating: 'Connaissance',
+    },
+    {
+        title: 'Drupal 8',
+        rating: 'Connaissance',
+    },
+    {
+        title: 'Symfony 5',
+        rating: 'Utilisation',
     },
 ]
 
 const formations = [
     {
-        title: 'Brevet Professionnel Esthétique',
-        school: 'CFA, Béthune',
-        date: 'Août 2009 à Décembre 2011',
+        title: 'Licence professionnelle Développement Internet et Mobile',
+        school: 'IUT, Calais (62250)',
+        date: '2019-2020',
         // description: 'Lorem ipsum dolor sit amet',
     },
     {
-        title: 'CAP Esthétique',
-        school: 'Lycée St Pierre, Calais',
-        date: 'Septembre 2007 à Juin 2009',
+        title: 'BTS SIO option SLAM',
+        school: 'Lycée Saint-Joseph, Saint-Martin-Boulogne (62280)',
+        date: 'Juin 2019',
         // description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+        title: 'Bac STI2D option SIN',
+        school: 'Lycée Édouard Branly, Boulogne-sur-Mer (62200)',
+        date: 'Juin 2017',
     },
 ]
 
 const experiences = [
     {
-        title: 'Responsable adjointe',
-        enterprise: 'Bijouterie Virginie, Outreau',
-        date: 'Mai 2016 à aujourd\'hui',
+        title: 'Développeur web',
+        enterprise: 'CDI, Diatem, Strasbourg (67000)',
+        date: '2020-2022',
         description:
-            "Management d'une équipe de 8 personnes, gestion des chiffres du magasin, saisie des commandes fournisseurs, gestion du stock, saisie des marchandises, appel des fournisseurs, contrôle des commandes et délais de livraison, saisie des factures, gestion du courrier, vente.",
+            'Plateforme spécialisée en PHP et VueJS, Site vitrine sous Drupal et Wordpress',
     },
     {
-        title: 'Gérante',
-        enterprise: 'Institut Perle Marine, Lumbres',
-        date: 'Avril 2014 à Mars 2016',
+        title: 'Stage en développement web',
+        enterprise: 'Stage, Armatis, Calais (62100)',
+        date: '2019',
         description:
-            "Gérante d'un institut de beauté, comptabilité, commandes fournisseurs, suivis des stocks, réception de la marchandise, saisies et suivis des stocks, soins esthétiques, vente."
+            "Création d'une application mobile en React-Native avec API développé en Laravel",
     },
     {
-        title: 'Conseillère vente',
-        enterprise: 'Terres et Eaux, Calais',
-        date: 'Avril 2012 à Février 2014',
+        title: 'Stage en développement web',
+        enterprise: 'Stage, Mes Concierges, Marquise (62250)',
+        date: '2018',
         description:
-            "Vente, réception de la marchandise, étiquetage, mise en rayon, suivis des stocks, conseils clients, mise en place d'un rayon, travail en équipe",
+            "Création d'une marketplace avec l'utilisation d'un framework PHP (Cocorico)",
+    },
+    {
+        title: 'Stage en développement web',
+        enterprise: "Stage, Mairie d'Echinghen, Echinghen (62360)",
+        date: '2017',
+        description: "Création d'un site vitrine en utilisant le CMS Wordpress",
     },
 ]
 
@@ -88,7 +123,7 @@ onMounted(() => {
         <div class="mx-auto max-w-4xl">
             <a href="/images/misc/cv.pdf" target="_blank">
                 <Button class="mb-7 text-center"
-                    >Télécharger la version PDF</Button
+                >Télécharger la version PDF</Button
                 >
             </a>
             <div class="block lg:hidden">
@@ -127,20 +162,23 @@ onMounted(() => {
                     <div class="flex flex-1 flex-col justify-center gap-3 pl-7">
                         <div class="text-2xl">
                             <span class="text-3xl uppercase"
-                                >Marine Arnoux</span
+                            >Guillaume Cazin</span
                             >
                         </div>
-<!--                        <div>
+                        <div>
                             <span class="text-4xl font-bold uppercase"
-                                >Développeur web</span
+                            >Développeur web</span
                             >
-                        </div>-->
+                        </div>
                         <div class="flex justify-between">
                             <div class="flex-1">
                                 <div class="block">
                                     <span
                                         class="border-before pb-2 font-bold uppercase"
-                                        >Contact</span
+                                    >Contact</span
+                                    >
+                                    <span class="inline font-bold text-blue-700"
+                                    >__</span
                                     >
                                 </div>
                                 <div class="mt-2 flex flex-col gap-1">
@@ -152,9 +190,9 @@ onMounted(() => {
                                         <Icon
                                             name="mail"
                                             :outline="false"
-                                            class="mr-1 text-indigo-700"
+                                            class="mr-1 text-blue-700"
                                         />
-                                        marinearnoux@outlook.fr
+                                        czn.guillaume@gmail.com
                                     </a>
                                     <a
                                         href="tel:06.10.85.42.18"
@@ -164,13 +202,86 @@ onMounted(() => {
                                         <Icon
                                             name="call"
                                             :outline="false"
-                                            class="mr-1 text-indigo-700"
+                                            class="mr-1 text-blue-700"
                                         />
-                                        06 51 08 32 25
+                                        +33 6 10 85 42 18
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="flex-1">
+                                <div class="block">
+                                    <span
+                                        class="border-before pb-2 font-bold uppercase"
+                                    >Social</span
+                                    >
+                                    <span class="inline font-bold text-blue-700"
+                                    >__</span
+                                    >
+                                </div>
+                                <div class="mt-2 flex flex-col gap-1">
+                                    <a
+                                        target="_blank"
+                                        href="https://www.linkedin.com/in/guillaume-cazin/"
+                                        class="flex items-center underline decoration-dotted"
+                                    >
+                                        <Icon
+                                            name="logo-linkedin"
+                                            class="mr-1"
+                                            :outline="false"
+                                        />
+                                        guillaume-cazin
+                                    </a>
+                                    <a
+                                        target="_blank"
+                                        href="http://github.com/gcazin"
+                                        class="flex items-center underline decoration-dotted"
+                                    >
+                                        <Icon
+                                            name="logo-github"
+                                            class="mr-1"
+                                            :outline="false"
+                                        />
+                                        gcazin
+                                    </a>
+                                    <a
+                                        target="_blank"
+                                        href="https://codepen.io/gcazin"
+                                        class="flex items-center underline decoration-dotted"
+                                    >
+                                        <Icon
+                                            name="logo-codepen"
+                                            class="mr-1"
+                                            :outline="false"
+                                        />
+                                        gcazin
                                     </a>
                                 </div>
                             </div>
                         </div>
+                        <div>
+                            <a
+                                class="font-bold uppercase text-blue-700"
+                                href="/cv"
+                            >www.guillaume-cazin.fr</a
+                            >
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mx-10 border-b">
+                    <div class="mb-2 block">
+                        <span
+                            class="border-before pb-2 text-xl font-bold uppercase"
+                        ><Icon name="person" class="mr-1 text-blue-700" /> A
+                            propos</span
+                        >
+                        <span class="inline font-bold text-blue-700"> __</span>
+                    </div>
+                    <div class="mb-1 bg-white pb-2">
+                        Passionné depuis 12 ans par tout ce qui est attrait au
+                        développement web, j'y ai consacré l'entièreté de mes
+                        années d'études et la majorité de mon temps libre durant
+                        toutes ces années.
                     </div>
                 </div>
 
@@ -182,11 +293,14 @@ onMounted(() => {
                                 <div class="mb-2 block">
                                     <span
                                         class="border-before pb-2 text-lg font-bold uppercase"
-                                        ><Icon
-                                            name="code-slash"
-                                            class="mr-1 text-indigo-700"
-                                        />
+                                    ><Icon
+                                        name="code-slash"
+                                        class="mr-1 text-blue-700"
+                                    />
                                         Compétences</span
+                                    >
+                                    <span class="inline font-bold text-blue-700"
+                                    >__</span
                                     >
                                 </div>
                                 <div
@@ -199,6 +313,23 @@ onMounted(() => {
                                     <span>
                                         {{ skill.title }}
                                     </span>
+                                    <div class="flex items-center">
+                                        <span
+                                            :class="{
+                                                'bg-green-50':
+                                                    skill.rating === 'Maîtrise',
+                                                'bg-orange-50':
+                                                    skill.rating ===
+                                                    'Connaissance',
+                                                'bg-red-50':
+                                                    skill.rating ===
+                                                    'Utilisation',
+                                            }"
+                                            class="w-fit whitespace-nowrap rounded-full px-2 py-1 text-center align-baseline text-sm font-bold leading-none transition-colors"
+                                        >
+                                            {{ skill.rating }}
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                             <hr />
@@ -206,14 +337,14 @@ onMounted(() => {
                                 <div class="block">
                                     <span
                                         class="border-before pb-2 text-lg font-bold uppercase"
-                                        ><Icon
-                                            name="language"
-                                            class="mr-1 text-indigo-700"
-                                        />
+                                    ><Icon
+                                        name="language"
+                                        class="mr-1 text-blue-700"
+                                    />
                                         Langues</span
                                     >
-                                    <span class="inline font-bold text-indigo-700"
-                                        >__</span
+                                    <span class="inline font-bold text-blue-700"
+                                    >__</span
                                     >
                                 </div>
                                 <div
@@ -228,9 +359,29 @@ onMounted(() => {
                                     <div class="flex w-full">
                                         <div class="flex-1">Anglais</div>
                                         <div class="flex-1 text-end">
-                                            Scolaire
+                                            Confirmé
                                         </div>
                                     </div>
+                                </div>
+                            </div>
+                            <hr />
+                            <div class="flex-1">
+                                <div class="block">
+                                    <span
+                                        class="border-before pb-2 text-lg font-bold uppercase"
+                                    ><Icon
+                                        name="library"
+                                        class="mr-1 text-blue-700"
+                                    />
+                                        Loisirs</span
+                                    >
+                                    <span class="inline font-bold text-blue-700"
+                                    >__</span
+                                    >
+                                </div>
+                                <div class="mt-3 bg-white">
+                                    🚲Vélo, 👨‍💻programmation, 🎮jeux-vidéo,
+                                    🪐astronomie
                                 </div>
                             </div>
                         </div>
@@ -242,14 +393,14 @@ onMounted(() => {
                                 <div class="mb-2 block">
                                     <span
                                         class="border-before pb-2 text-lg font-bold uppercase"
-                                        ><Icon
-                                            name="briefcase"
-                                            class="mr-1 text-indigo-700"
-                                        />
+                                    ><Icon
+                                        name="briefcase"
+                                        class="mr-1 text-blue-700"
+                                    />
                                         Expériences professionnelles</span
                                     >
-                                    <span class="inline font-bold text-indigo-700"
-                                        >__</span
+                                    <span class="inline font-bold text-blue-700"
+                                    >__</span
                                     >
                                 </div>
                                 <div class="flex flex-col gap-2 bg-white">
@@ -293,14 +444,14 @@ onMounted(() => {
                                 <div class="mb-2 block">
                                     <span
                                         class="border-before pb-2 text-lg font-bold uppercase"
-                                        ><Icon
-                                            name="school"
-                                            class="mr-1 text-indigo-700"
-                                        />
+                                    ><Icon
+                                        name="school"
+                                        class="mr-1 text-blue-700"
+                                    />
                                         Formations & diplômes</span
                                     >
-                                    <span class="inline font-bold text-indigo-700"
-                                        >__</span
+                                    <span class="inline font-bold text-blue-700"
+                                    >__</span
                                     >
                                 </div>
                                 <div class="flex flex-col gap-2 bg-white">
@@ -348,4 +499,15 @@ onMounted(() => {
     background-color: #ffffff;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='199' viewBox='0 0 100 199'%3E%3Cg fill='%231d4ed8' fill-opacity='0.05'%3E%3Cpath d='M0 199V0h1v1.99L100 199h-1.12L1 4.22V199H0zM100 2h-.12l-1-2H100v2z'%3E%3C/path%3E%3C/g%3E%3C/svg%3E");
 }
+/*.border-before {
+  position: relative;
+}
+.border-before::before {
+  content: '';
+  height: 4px;
+  width: 100px;
+  background: rgb(59 130 246);
+  position: absolute;
+  bottom: -2px;
+}*/
 </style>
